e7b8291591115f43b6592e758ede4b351d129fd1
CGU-B1029048 <119442447+CGU-B1029048@users.noreply.github.com>
2022-12-02 17:12:45 +0800

    Update clock.v

--- midterm/clock.v
+++ midterm/clock.v
@@ -1,30 +1,29 @@
-module clock(clk, acc_b, clock_out);
-    input clk, acc_b;
+module clock(clk, acc, clock_out);
+    input clk, acc;
     output reg clock_out;
     reg S;
     reg [25:0] counter;
-    wire acc;
     parameter clk_freq1 = 2500000;
     parameter clk_freq0 = 25000000;
 
-    debounce ACCB(clk, acc_b, acc);
-
-    always@(posedge acc) begin
-        S <= ~S;
+    always@(posedge clk) begin
+        if (acc)
+		      S <= ~S;
     end
+	 
     always@(posedge clk) begin
         case(S)
         0: begin
-            if (counter == clk_freq0) begin
-                counter <= 0;
-                clock_out <= ~clock_out
+            if (counter >= clk_freq0) begin
+                counter <= 1;
+                clock_out <= ~clock_out;
             end else
                 counter <= counter + 1;
         end
         1: begin
-            if (counter == clk_freq2) begin
-                counter <= 0;
-                clock_out <= ~clock_out
+            if (counter >= clk_freq1) begin
+                counter <= 1;
+                clock_out <= ~clock_out;
             end else
                 counter <= counter + 1;
         end
