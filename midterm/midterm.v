a9dfeacaf3734cefc2f9d2e25da5d83771b27495
CGU-B1029048 <119442447+CGU-B1029048@users.noreply.github.com>
2022-12-02 18:24:40 +0800

    teacher final

--- midterm/midterm.v
+++ midterm/midterm.v
@@ -1,8 +1,8 @@
 module midterm(clk, pause_b, acc_b, seg0, seg1, seg2, seg3);
     input clk, pause_b, acc_b;
     output [6:0] seg0, seg1, seg2, seg3;
-	reg pause, start, small_counter;
-    reg [11:0] timer=1;
+	 reg pause, small_counter;
+    reg [11:0] timer;
     wire pause_w, acc_w, custom_clk, blink;
 	wire [3:0] s0, s1, m0, m1;
 	//debug
@@ -23,6 +23,11 @@ module midterm(clk, pause_b, acc_b, seg0, seg1, seg2, seg3);
             pause <= ~pause;
         else if (!timer && !small_counter)
             pause <= 1;
+				
+		if (!timer)
+			 small_counter <= 1;
+		else
+			 small_counter <= 0;
 	end
 
     always@(posedge custom_clk) begin
@@ -32,11 +37,9 @@ module midterm(clk, pause_b, acc_b, seg0, seg1, seg2, seg3);
             end
             else begin
                 timer <= timer - 1;
-                small_counter <= 0;
             end
         end
 		  
-		  if (!timer)
-			 small_counter <= 1;
+		  
     end
 endmodule
