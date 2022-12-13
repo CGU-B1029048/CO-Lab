d6e47cee8e3e252ad5dc6245a79cda05726d2ed6
CGU-B1029048 <119442447+CGU-B1029048@users.noreply.github.com>
2022-12-13 16:02:07 +0800

    Update lab6.v

--- lab6/lab6.v
+++ lab6/lab6.v
@@ -2,19 +2,21 @@ module lab6 (
     input [7:0] A, B,
     input Cin, clk,
     input [2:0] S,
-    output reg [7:0] Data
-    output Cout;
-);
+    output reg [7:0] Data,
+    output Cout
+    );
+    reg [7:0] AddB;
 
     always@(posedge clk) begin
         case(S[2])
             0:begin
                 case (S[1:0])
-                    0: Data = A + Cin; 
-                    1: Data = A + B + Cin; 
-                    2: Data = A + ~B + Cin; 
-                    3: Data = A - 1 + Cin; 
+                    0: AddB = 8'b0;
+                    1: AddB = B;
+                    2: AddB = ~B;
+                    3: AddB = 8'b11111111;
                 endcase
+                Data = A + AddB + Cin;
             end
             1: begin
                 case({S[0], Cin})
